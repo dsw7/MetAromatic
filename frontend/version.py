ffd5337601554ec32fd2388a9b6da9b60293b01b
David Weber <dsw7@sfu.ca>
2019-12-11 06:59:37 +0000

    Remove extra .gitignore

--- frontend/version.py
+++ frontend/version.py
@@ -1,1 +1,1 @@
-version = {'__version__': '19.91', 'UUID': '61e7417afe2746c68ea776d17a93f631', 'sha': 'f9b7d98d31f74c5d5d844f2e0660805de329cb84', 'message': 'Fix version path'}
+version = {'__version__': '19.122', 'UUID': 'b0ac9323c65c474faec4545f8f40f47c', 'sha': 'c5c8fc6c318eebd40fb5b5ea8941e51f19a242b4', 'message': 'Make even'}
