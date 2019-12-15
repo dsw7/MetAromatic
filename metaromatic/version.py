0ce21befd0c1afa1b51f9ac32d5248d61c993f08
David Weber <dsw7@sfu.ca>
2019-12-15 08:33:28 +0000

    Delete readme

--- metaromatic/version.py
+++ metaromatic/version.py
@@ -1,1 +1,1 @@
-version = {'__version__': '19.77', 'UUID': 'd281b360539d4edea482d56a52c18eb9', 'sha': 'ae6a6a53a1b3deabb8d72216027393952ad78cb5', 'message': 'Refactor testing framework'}
+version = {'__version__': '19.134', 'UUID': '8e90f58ae9e241ecb5115c6223decd23', 'sha': 'c5bc4b617076743a7163260f565d1503af42d179', 'message': 'Update README.md'}
