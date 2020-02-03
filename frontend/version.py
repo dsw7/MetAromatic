dd72b999ca646636cced8bfbd8467528bc85cf56
David Weber <dsw7@sfu.ca>
2020-02-03 07:46:41 +0000

    Remove old files

--- frontend/version.py
+++
@@ -1,1 +0,0 @@
-version = {'__version__': '19.140', 'UUID': '1280283eb73d4941bb2f1bdd7b662d18', 'sha': '8458c57658a4e6ee64d84dd26dc3079235a634b9', 'message': 'Update README.md'}
--- metaromatic/version.py
+++
@@ -1,1 +0,0 @@
-version = {'__version__': '20.147', 'UUID': '225d0fb03604423db811880a0163fb68', 'sha': 'ea30c3692381444d09523354890bba020e6d1a6f', 'message': 'Separate tests'}
