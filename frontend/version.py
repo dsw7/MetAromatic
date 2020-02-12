f40d74ac5d098a31eef079e635ae4d126f1d1792
David Weber <dsw7@sfu.ca>
2020-02-12 05:29:31 +0000

    Version file

--- frontend/version.py
+++ frontend/version.py
@@ -1,1 +1,1 @@
-version = {'__version__': '19.140', 'UUID': '1280283eb73d4941bb2f1bdd7b662d18', 'sha': '8458c57658a4e6ee64d84dd26dc3079235a634b9', 'message': 'Update README.md'}
+version = {'__version__': '20.149', 'UUID': 'db82ea49eb964f53a5f824e1e693d852', 'sha': '99e335d8295a145d9cadcf0c388ad7fc079910a5', 'message': 'Update README.md'}
