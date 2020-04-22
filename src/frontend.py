8a7e55288de42a1139be55a0d2ba5b710479fdd6
David Weber <dsw7@sfu.ca>
2020-04-22 08:16:40 +0100

    Update metavars

--- src/frontend.py
+++ src/frontend.py
@@ -23,9 +23,9 @@ def get_command_line_arguments():
     constants = get_defaults()
     parser = ArgumentParser(formatter_class=RawTextHelpFormatter)
     job_type = parser.add_mutually_exclusive_group()
-    job_type.add_argument('--ai', help=help_messages.MSG_AI, dest='single_aromatic_interaction_query')
-    job_type.add_argument('--bi', help=help_messages.MSG_BI, dest='single_bridging_interaction_query')
-    job_type.add_argument('--batch', help=help_messages.MSG_BATCH, dest='run_batch_job')
+    job_type.add_argument('--ai', help=help_messages.MSG_AI, dest='single_aromatic_interaction_query', metavar='<pdb-code>')
+    job_type.add_argument('--bi', help=help_messages.MSG_BI, dest='single_bridging_interaction_query', metavar='<pdb-code>')
+    job_type.add_argument('--batch', help=help_messages.MSG_BATCH, dest='run_batch_job', metavar='</path/to/file>')
     job_type.add_argument('--test', help=help_messages.MSG_TEST, action='store_true', dest='run_tests')
     job_type.add_argument('--testcov', help=help_messages.MSG_TEST_COV, action='store_true', dest='run_tests_with_coverage')
 
@@ -33,73 +33,84 @@ def get_command_line_arguments():
         '--cutoff_distance',
         help=help_messages.MSG_CUTOFF,
         default=constants['default_cutoff_distance'],
-        type=float
+        type=float,
+        metavar='<distance-in-Angstroms>'
     )
 
     parser.add_argument(
         '--cutoff_angle',
         help=help_messages.MSG_ANGLE,
         default=constants['default_cutoff_angle'],
-        type=float
+        type=float,
+        metavar='<degrees>'
     )
 
     parser.add_argument(
         '--model',
         help=help_messages.MSG_MODEL,
         default=constants['default_model'],
-        type=str
+        type=str,
+        metavar='<cp|rm>'
     )
 
     parser.add_argument(
         '--chain',
         help=help_messages.MSG_CHAIN,
         default=constants['default_chain'],
-        type=str
+        type=str,
+        metavar='<A|B|C|...>'
     )
 
     parser.add_argument(
         '--vertices',
         help=help_messages.MSG_VERTICES,
         default=constants['default_vertices'],
-        type=int
+        type=int,
+        metavar='<3|4|5...>'
     )
 
     parser.add_argument(
         '--host',
         help=help_messages.MSG_HOST,
-        default=constants['default_mongodb_host']
+        default=constants['default_mongodb_host'],
+        metavar='<ip-address>'
     )
 
     parser.add_argument(
         '--port',
         help=help_messages.MSG_PORT,
         default=constants['default_mongodb_port'],
-        type=int
+        type=int,
+        metavar='<port>'
     )
 
     parser.add_argument(
         '--database',
         help=help_messages.MSG_DB,
-        default=constants['default_mongodb_database']
+        default=constants['default_mongodb_database'],
+        metavar='<database-name>'
     )
 
     parser.add_argument(
         '--collection',
         help=help_messages.MSG_COL,
-        default=constants['default_mongodb_collection']
+        default=constants['default_mongodb_collection'],
+        metavar='<collection-name>'
     )
 
     parser.add_argument(
         '--threads',
         help=help_messages.MSG_THREADS,
         default=constants['default_threads'],
-        type=int
+        type=int,
+        metavar='<number-threads>'
     )
 
     parser.add_argument(
         '--test-expression',
         help=help_messages.MSG_TEST_EXPRESSION,
-        dest='test_expression'
+        dest='test_expression',
+        metavar='<Test*|test*>'
     )
 
     parser.add_argument(
--- src/utilities/help_messages.py
+++ src/utilities/help_messages.py
@@ -1,25 +1,31 @@
 #### top level mutually exclusive options
-MSG_AI = 'Run a single Met-aromatic calculation on a PDB structure. \nUsage: $ python runner.py --ai <pdb-code>'
-MSG_BI = 'Run a single bridging interaction query on a PDB structure. \nUsage: $ python runner.py --bi <pdb-code>'
-MSG_BATCH = 'Run a batch job. \nUsage: $ python runner.py --batch <path/to/batch/file>'
-MSG_TEST = 'Run pytest over this project. \nUsage: $ python runner.py --test'
-MSG_TEST_COV = 'Run pytest over this project with a coverage report. \nUsage: $ python runner.py --testcov'
+MSG_AI = 'Run a single Met-aromatic calculation on a PDB structure.'
+MSG_BI = 'Run a single bridging interaction query on a PDB structure.'
+MSG_BATCH = 'Run a batch job.'
+MSG_TEST = 'Run Pytest over this project.'
+MSG_TEST_COV = 'Run Pytest over this project with a coverage report.'
 
 #### low level met aromatic algorithm options for ai, bi and batch jobs
-MSG_CUTOFF = 'Set a Euclidean cutoff. \nUsage: --cutoff <float>'
-MSG_ANGLE = 'Set Met-theta/Met-phi angle. \nUsage: --angle <float>'
-MSG_MODEL = 'Set a lone pair interpolation model. \nUsage: --model <cp|rm>'
-MSG_CHAIN = 'Choose a chain. \nUsage: --chain <chain>'
-MSG_VERTICES = 'Choose number of vertices for bridging interactions. \nUsage: --vertices <3|4|5...>'
+MSG_CUTOFF = 'Set a Euclidean cutoff.'
+MSG_ANGLE = 'Set Met-theta/Met-phi angle.'
+MSG_MODEL = 'Set a lone pair interpolation model.'
+MSG_CHAIN = 'Choose a protein chain identifier.'
+MSG_VERTICES = 'Choose number of vertices for bridging interactions.'
 
 #### mongodb batch job parameters
-MSG_PORT = 'Set a MongoDB port. \nUsage: --mongoport <port>'
-MSG_HOST = 'Set a MongoDB host. \nUsage: --mongohost <host>'
-MSG_DB = 'Choose a MongoDB export database name. \nUsage: --database <name>'
-MSG_COL = 'Choose a MongoDB export collection name. \nUsage: --collection <name>'
-MSG_THREADS = 'Choose number of threads to use for batch job. \nUsage: --threads <num_threads>'
+MSG_PORT = 'Set a MongoDB port.'
+MSG_HOST = 'Set a MongoDB host.'
+MSG_DB = 'Choose a MongoDB export database name.'
+MSG_COL = 'Choose a MongoDB export collection name.'
+MSG_THREADS = (
+    'Choose number of threads to use for batch job.\n'
+    'Default number of threads is 5.'
+)
 
 #### testing
-MSG_TEST_EXPRESSION = 'Run a subset of tests matching expression. \nUsage: --test-expression <Test*|test*>'
-MSG_EXIT_ON_FAILURE = 'Exit on first failure. \nUsage: --exit-on-failure'
-MSG_QUIET = 'Disable test verbosity. \nUsage: --quiet'
+MSG_TEST_EXPRESSION = (
+    'Run a subset of tests matching expression.\n'
+    'Expression can match filename, test class name or test function name.'
+)
+MSG_EXIT_ON_FAILURE = 'Exit on first test failure.'
+MSG_QUIET = 'Disable test verbosity.'
