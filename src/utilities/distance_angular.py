ee108bf7f1afb3876e767476f18f3068c3abb944
David Weber <dsw7@sfu.ca>
2020-05-14 05:38:57 +0100

    Return dict from distance_angular

--- src/utilities/distance_angular.py
+++ src/utilities/distance_angular.py
@@ -1,19 +1,28 @@
 from numpy.linalg import norm
 from .math_utils.cross_product_method import vector_angle
 
+ROUND_TO_NUMBER = 3
+
 def apply_distance_angular_condition(midpoints, lone_pairs, cutoff_distance, cutoff_angle):
-    pairs = []    
+    results = []
     for dict_met in lone_pairs:
         for midpoint in midpoints:
             vector_v = midpoint[2] - dict_met['coords_sd']
             norm_v = norm(vector_v)
-            if norm_v <= cutoff_distance:  # distance condition
+            if norm_v <= cutoff_distance:
                 met_theta = vector_angle(vector_v, dict_met['vector_a'])
                 met_phi = vector_angle(vector_v, dict_met['vector_g'])
-                if (met_theta <= cutoff_angle) or (met_phi <= cutoff_angle):  # angular condition
-                    pairs.append([midpoint[1], midpoint[0], 'MET', dict_met['position'], norm_v, met_theta, met_phi])
+                if (met_theta <= cutoff_angle) or (met_phi <= cutoff_angle):
+                    results.append({
+                        'aromatic_residue': midpoint[1],
+                        'aromatic_position': int(midpoint[0]),
+                        'methionine_position': int(dict_met['position']),
+                        'norm': round(norm_v, ROUND_TO_NUMBER),
+                        'met_theta_angle': round(met_theta, ROUND_TO_NUMBER),
+                        'met_phi_angle': round(met_phi, ROUND_TO_NUMBER)
+                    })
                 else:
                     pass
             else:
                 pass
-    return pairs
+    return results
