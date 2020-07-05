9363cb5ba3fef929d359073639070bd896be11b7
David Weber <dsw7@sfu.ca>
2020-07-05 05:31:24 +0100

    Update errors tests

--- src/conftest.py
+++ src/conftest.py
@@ -1,11 +1,11 @@
 from os import path
 from pytest import fixture
 
-@fixture
-def default_met_aromatic_parameters(scope='session'):
-    return {
-        'distance': 4.9,
-        'angle': 109.5,
-        'chain': 'A',
-        'model': 'cp',
-    }
+#@fixture
+#def default_met_aromatic_parameters(scope='session'):
+#    return {
+#        'distance': 4.9,
+#        'angle': 109.5,
+#        'chain': 'A',
+#        'model': 'cp',
+#    }
--- src/test_errors.py
+++ src/test_errors.py
@@ -2,61 +2,65 @@ from met_aromatic import MetAromatic
 from utilities import errors
 
 
-def test_invalid_distance_error(default_met_aromatic_parameters):
-    assert MetAromatic(
-        code='foobarbaz',
-        cutoff_angle=default_met_aromatic_parameters['angle'],
-        cutoff_distance=0.00,
-        model=default_met_aromatic_parameters['model'],
-        chain=default_met_aromatic_parameters['chain']
-    ).get_met_aromatic_interactions()['exit_code'] == errors.ErrorCodes.InvalidCutoffsError
+class TestErrors:
+    def setup_class(self):
+        self.default_parameters = {
+            'distance': 4.9,
+            'angle': 109.5,
+            'chain': 'A',
+            'model': 'cp',
+        }
 
+    def test_invalid_distance_error(self):
+        assert MetAromatic(
+            code='foobarbaz',
+            cutoff_angle=self.default_parameters['angle'],
+            cutoff_distance=0.00,
+            model=self.default_parameters['model'],
+            chain=self.default_parameters['chain']
+        ).get_met_aromatic_interactions()['exit_code'] == errors.ErrorCodes.InvalidCutoffsError
 
-def test_invalid_angle_error(default_met_aromatic_parameters):
-    assert MetAromatic(
-        code='foobarbaz',
-        cutoff_angle=-720.00,
-        cutoff_distance=default_met_aromatic_parameters['distance'],
-        model=default_met_aromatic_parameters['model'],
-        chain=default_met_aromatic_parameters['chain']
-    ).get_met_aromatic_interactions()['exit_code'] == errors.ErrorCodes.InvalidCutoffsError
+    def test_invalid_angle_error(self):
+        assert MetAromatic(
+            code='foobarbaz',
+            cutoff_angle=-720.00,
+            cutoff_distance=self.default_parameters['distance'],
+            model=self.default_parameters['model'],
+            chain=self.default_parameters['chain']
+        ).get_met_aromatic_interactions()['exit_code'] == errors.ErrorCodes.InvalidCutoffsError
 
+    def test_invalid_pdb_code_error(self):
+        assert MetAromatic(
+            code='foobarbaz',
+            cutoff_angle=self.default_parameters['angle'],
+            cutoff_distance=self.default_parameters['distance'],
+            model=self.default_parameters['model'],
+            chain=self.default_parameters['chain']
+        ).get_met_aromatic_interactions()['exit_code'] == errors.ErrorCodes.InvalidPDBFileError
 
-def test_invalid_pdb_code_error(default_met_aromatic_parameters):
-    assert MetAromatic(
-        code='foobarbaz',
-        cutoff_angle=default_met_aromatic_parameters['angle'],
-        cutoff_distance=default_met_aromatic_parameters['distance'],
-        model=default_met_aromatic_parameters['model'],
-        chain=default_met_aromatic_parameters['chain']
-    ).get_met_aromatic_interactions()['exit_code'] == errors.ErrorCodes.InvalidPDBFileError
+    def test_no_met_coordinates_error(self):
+        assert MetAromatic(
+            code='3nir',
+            cutoff_angle=self.default_parameters['angle'],
+            cutoff_distance=self.default_parameters['distance'],
+            model=self.default_parameters['model'],
+            chain=self.default_parameters['chain']
+        ).get_met_aromatic_interactions()['exit_code'] == errors.ErrorCodes.NoMetCoordinatesError
 
+    def test_invalid_model_error(self):
+        assert MetAromatic(
+            code='1rcy',
+            cutoff_angle=self.default_parameters['angle'],
+            cutoff_distance=self.default_parameters['distance'],
+            model='foobarbaz',
+            chain=self.default_parameters['chain']
+        ).get_met_aromatic_interactions()['exit_code'] == errors.ErrorCodes.InvalidModelError
 
-def test_no_met_coordinates_error(default_met_aromatic_parameters):
-    assert MetAromatic(
-        code='3nir',
-        cutoff_angle=default_met_aromatic_parameters['angle'],
-        cutoff_distance=default_met_aromatic_parameters['distance'],
-        model=default_met_aromatic_parameters['model'],
-        chain=default_met_aromatic_parameters['chain']
-    ).get_met_aromatic_interactions()['exit_code'] == errors.ErrorCodes.NoMetCoordinatesError
-
-
-def test_invalid_model_error(default_met_aromatic_parameters):
-    assert MetAromatic(
-        code='1rcy',
-        cutoff_angle=default_met_aromatic_parameters['angle'],
-        cutoff_distance=default_met_aromatic_parameters['distance'],
-        model='foobarbaz',
-        chain=default_met_aromatic_parameters['chain']
-    ).get_met_aromatic_interactions()['exit_code'] == errors.ErrorCodes.InvalidModelError
-
-
-def test_no_results_error(default_met_aromatic_parameters):
-    assert MetAromatic(
-        code='1a5r',
-        cutoff_angle=default_met_aromatic_parameters['angle'],
-        cutoff_distance=default_met_aromatic_parameters['distance'],
-        model=default_met_aromatic_parameters['model'],
-        chain=default_met_aromatic_parameters['chain']
-    ).get_met_aromatic_interactions()['exit_code'] == errors.ErrorCodes.NoResultsError
+    def test_no_results_error(self):
+        assert MetAromatic(
+            code='1a5r',
+            cutoff_angle=self.default_parameters['angle'],
+            cutoff_distance=self.default_parameters['distance'],
+            model=self.default_parameters['model'],
+            chain=self.default_parameters['chain']
+        ).get_met_aromatic_interactions()['exit_code'] == errors.ErrorCodes.NoResultsError
