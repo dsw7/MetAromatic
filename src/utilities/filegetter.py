fae35a12595e324aa337863ed238bbbd6e3688f1
David Weber <dsw7@sfu.ca>
2020-09-11 10:02:28 +0100

    Modify gzip handling

--- src/utilities/filegetter.py
+++ src/utilities/filegetter.py
@@ -5,15 +5,12 @@ from os import remove, path
 from tempfile import gettempdir
 
 
-TMP_DIR = gettempdir()
-
-
 class PDBFileGetter:
     def __init__(self, code):
         self.code = code.lower()
         tar = f'pdb{self.code}.ent.gz'
         self.url = f'ftp://ftp.wwpdb.org/pub/pdb/data/structures/divided/pdb/{self.code[1:3]}/{tar}'
-        self.res_tar = path.join(TMP_DIR, tar)
+        self.res_tar = path.join(gettempdir(), tar)
         self.res_untar = self.res_tar.strip('.gz')
 
     def fetch_entry_from_pdb(self):
@@ -21,13 +18,14 @@ class PDBFileGetter:
             urlcleanup()
             urlretrieve(self.url, self.res_tar)
         except URLError:
-            pass
-        else:
-            with gzip.open(self.res_tar, 'rb') as gz:
-                with open(self.res_untar, 'wb') as out:
-                    out.writelines(gz)
-            remove(self.res_tar)
-            return self.res_untar
+            return None
+
+        with gzip.open(self.res_tar, 'rb') as gz:
+            with open(self.res_untar, 'wb') as out:
+                out.writelines(gz)
+
+        remove(self.res_tar)
+        return self.res_untar
 
     def remove_entry(self):
         retbool = True
