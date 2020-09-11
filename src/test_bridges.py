649fa81904b8a73108d503a51e13b6f19f78d8c1
David Weber <dsw7@sfu.ca>
2020-09-11 06:10:53 +0100

    Remove more errors

--- src/test_bridges.py
+++ src/test_bridges.py
@@ -2,14 +2,12 @@ from json import loads
 from os import path
 from pytest import mark, skip, exit
 from met_aromatic import MetAromatic
-from utilities import errors
-
+from consts import EXIT_FAILURE
 
 CONTROL_BRIDGE_DATA = path.join(
     path.dirname(path.abspath(__file__)),
     './test_data/test_n_3_bridges_no_ang_limit_6_angstroms.json'
 )
-
 
 def get_control_bridges(file, size=100):
     try:
@@ -17,15 +15,14 @@ def get_control_bridges(file, size=100):
             data = [loads(line) for line in jsonfile]
     except FileNotFoundError:
         exit(f'File {file} is missing.')
-    else:
-        outgoing = []
-        for datum in data[0:size]:
-            outgoing.append({
-                'pdb_code': datum.get('code'),
-                'bridge': datum.get('bridge')
-            })
-        return outgoing
 
+    outgoing = []
+    for datum in data[0:size]:
+        outgoing.append({
+            'pdb_code': datum.get('code'),
+            'bridge': datum.get('bridge')
+        })
+    return outgoing
 
 def get_control_bridge_test_ids(file, size=100):
     try:
@@ -33,11 +30,11 @@ def get_control_bridge_test_ids(file, size=100):
             data = [loads(line) for line in jsonfile]
     except FileNotFoundError:
         exit(f'File {file} is missing.')
-    else:
-        outgoing = []
-        for datum in data[0:size]:
-            outgoing.append(datum.get('code').lower())
-        return outgoing
+
+    outgoing = []
+    for datum in data[0:size]:
+        outgoing.append(datum.get('code').lower())
+    return outgoing
 
 
 class TestBridges:
@@ -74,18 +71,18 @@ class TestBridges:
     @mark.parametrize(
         'code, cutoff_distance, cutoff_angle, error',
         [
-            ('1rcy', 0.00, 109.5, errors.ErrorCodes.InvalidCutoffsError),
-            ('1rcy', 4.95, 720.0, errors.ErrorCodes.InvalidCutoffsError),
-            ('2rcy', 4.95, 109.5, errors.ErrorCodes.NoMetCoordinatesError),
-            ('3nir', 4.95, 109.5, errors.ErrorCodes.NoMetCoordinatesError),
-            ('abcd', 4.95, 109.5, errors.ErrorCodes.InvalidPDBFileError)
+            ('1rcy', 0.00, 109.5, EXIT_FAILURE),
+            ('1rcy', 4.95, 720.0, EXIT_FAILURE),
+            ('2rcy', 4.95, 109.5, EXIT_FAILURE),
+            ('3nir', 4.95, 109.5, EXIT_FAILURE),
+            ('abcd', 4.95, 109.5, EXIT_FAILURE),
         ],
         ids=[
-            "Testing errors.ErrorCodes.InvalidCutoffsError",
-            "Testing errors.ErrorCodes.InvalidCutoffsError",
-            "Testing errors.ErrorCodes.NoMetCoordinatesError",
-            "Testing errors.ErrorCodes.NoMetCoordinatesError",
-            "Testing errors.ErrorCodes.InvalidPDBFileError"
+            "Testing InvalidCutoffsError",
+            "Testing InvalidCutoffsError",
+            "Testing NoMetCoordinatesError",
+            "Testing NoMetCoordinatesError",
+            "Testing InvalidPDBFileError"
         ]
     )
     def test_no_bridges_response(self, code, cutoff_distance, cutoff_angle, error):
