f8c780260e619b15c357070ab21931533af5ebd2
David Weber <dsw7@sfu.ca>
2020-09-12 04:45:13 +0100

    Remove ws

--- utils/utilities/math_utils/test_get_hexagon_midpoints.py
+++ utils/utilities/math_utils/test_get_hexagon_midpoints.py
@@ -1,6 +1,5 @@
 from .get_hexagon_midpoints import get_hexagon_midpoints
 
-
 HEXAGON_COORDINATES = [
     (0.866, 0.5, 0.0),
     (0.866, -0.5, 0.0),
@@ -10,7 +9,6 @@ HEXAGON_COORDINATES = [
     (0.0, 1.0, 0.0)
 ]
 
-
 HEXAGON_MIDPOINTS = {
     (0.433, 0.75, 0.0),
     (-0.433, 0.75, 0.0),
@@ -19,7 +17,6 @@ HEXAGON_MIDPOINTS = {
     (0.433, -0.75, 0.0),
     (0.866, 0.0, 0.0)
 }
-
 
 def test_get_hexagon_midpoints():
     coordinates = [list(i) for i in zip(*HEXAGON_COORDINATES)]
