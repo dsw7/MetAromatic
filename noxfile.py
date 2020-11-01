42f69e3c69678b780c744804ebd4430809fe32d7
David Weber <dsw7@sfu.ca>
2020-11-01 11:37:30 +0000

    Fix bad paths in noxfile

--- noxfile.py
+++ noxfile.py
@@ -2,26 +2,26 @@ from os import path
 from tempfile import gettempdir
 import nox
 
-DEPENDENCIES_TESTS = [
+PATH_TO_PROJECT = path.dirname(__file__)
+PYTHON_INTERP_VERSION = '3.6.10'
+ALL_PROJECT_DEPENDENCIES = [
     'pytest==5.2.1',
     'networkx==2.3',
     'numpy==1.19.0',
     'pymongo==3.9.0',
-    'click==7.1.2'
-]
-DEPENDENCIES_PYLINT = [
+    'click==7.1.2',
     'pylint==2.4.2'
 ]
-PYTHON_INTERP_VERSION = '3.6.10'
 
+nox.options.report = path.join(gettempdir(), 'noxreport')
 nox.options.envdir = path.join(gettempdir(), '.nox')
 
 @nox.session(python=PYTHON_INTERP_VERSION)
 def lint(session):
-    session.install(*DEPENDENCIES_PYLINT)
-    session.run('pylint', '*py')
+    session.install(*ALL_PROJECT_DEPENDENCIES)
+    session.run('pylint', PATH_TO_PROJECT, '--output-format=colorized')
 
 @nox.session(python=PYTHON_INTERP_VERSION)
 def tests(session):
-    session.install(*DEPENDENCIES_TESTS)
-    session.run('pytest', '-vs')
+    session.install(*ALL_PROJECT_DEPENDENCIES)
+    session.run('pytest', '-vs', PATH_TO_PROJECT)
