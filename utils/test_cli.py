9d9ebcc964da1c24a9aa3f1937b8ee3a6e2e868f
David Weber <dsw7@sfu.ca>
2021-06-02 08:55:30 +0100

    Fix tests

--- utils/test_cli.py
+++ utils/test_cli.py
@@ -15,7 +15,7 @@ class TestCommandLineInterface:
 
     def test_aromatic_interaction_working_query(self):
         assert call(
-            f'{self.path_runner} single-met-aromatic-query 1rcy'.split(),
+            f'{self.path_runner} pair 1rcy'.split(),
             stdout=DEVNULL
         ) == EXIT_SUCCESS
 
@@ -29,7 +29,7 @@ class TestCommandLineInterface:
         pwd = getcwd()
         chdir(path.dirname(path.dirname(self.path_runner)))
         retval = call(
-            f'MetAromatic/runner.py single-met-aromatic-query 1rcy'.split(),
+            f'MetAromatic/runner.py pair 1rcy'.split(),
             stdout=DEVNULL
         )
         chdir(pwd)
@@ -39,7 +39,7 @@ class TestCommandLineInterface:
         pwd = getcwd()
         chdir(path.join(path.dirname(self.path_runner), 'utils/'))
         retval = call(
-            f'../runner.py single-met-aromatic-query 1rcy'.split(),
+            f'../runner.py pair 1rcy'.split(),
             stdout=DEVNULL
         )
         chdir(pwd)
@@ -49,7 +49,7 @@ class TestCommandLineInterface:
         pwd = getcwd()
         chdir(path.join(path.dirname(self.path_runner), 'utils/primitives'))
         retval = call(
-            f'../../runner.py single-met-aromatic-query 1rcy'.split(),
+            f'../../runner.py pair 1rcy'.split(),
             stdout=DEVNULL
         )
         chdir(pwd)
@@ -99,60 +99,60 @@ class TestCommandLineInterface:
 
     def test_invalid_cutoff_distance(self):
         assert call(
-            f'{self.path_runner} single-met-aromatic-query 1rcy --cutoff-distance 0.00'.split(),
+            f'{self.path_runner} pair 1rcy --cutoff-distance 0.00'.split(),
             stdout=DEVNULL
         ) == EXIT_FAILURE
 
     def test_invalid_cutoff_distance_stringified(self):
         assert call(
-            f'{self.path_runner} single-met-aromatic-query 1rcy --cutoff-distance foo'.split(),
+            f'{self.path_runner} pair 1rcy --cutoff-distance foo'.split(),
             stderr=DEVNULL
         ) == EXIT_GENERAL_PROGRAM_FAILURES
 
     def test_invalid_cutoff_angle(self):
         assert call(
-            f'{self.path_runner} single-met-aromatic-query 1rcy --cutoff-angle 361.00'.split(),
+            f'{self.path_runner} pair 1rcy --cutoff-angle 361.00'.split(),
             stdout=DEVNULL
         ) == EXIT_FAILURE
 
     def test_invalid_code(self):
         assert call(
-            f'{self.path_runner} single-met-aromatic-query foobar'.split(),
+            f'{self.path_runner} pair foobar'.split(),
             stdout=DEVNULL
         ) == EXIT_FAILURE
 
     def test_no_met_coordinates(self):
         assert call(
-            f'{self.path_runner} single-met-aromatic-query 3nir'.split(),
+            f'{self.path_runner} pair 3nir'.split(),
             stdout=DEVNULL
         ) == EXIT_FAILURE
 
     def test_invalid_model(self):
         assert call(
-            f'{self.path_runner} single-met-aromatic-query 1rcy --model foobarbaz'.split(),
+            f'{self.path_runner} pair 1rcy --model foobarbaz'.split(),
             stdout=DEVNULL
         ) == EXIT_FAILURE
 
     def test_no_results(self):
         assert call(
-            f'{self.path_runner} single-met-aromatic-query 1a5r'.split(),
+            f'{self.path_runner} pair 1a5r'.split(),
             stdout=DEVNULL
         ) == EXIT_FAILURE
 
     def test_bad_query_type(self):
         assert call(
-            f'{self.path_runner} single-met-aromatic-query 1rcy --query foobarbaz'.split(),
+            f'{self.path_runner} pair 1rcy --query foobarbaz'.split(),
             stderr=DEVNULL
         ) == EXIT_GENERAL_PROGRAM_FAILURES
 
     @mark.parametrize(
         'subquery',
         [
-            'single-met-aromatic-query ',
-            'single-met-aromatic-query 1rcy single-bridging-interaction-query 1rcy',
-            'single-met-aromatic-query 1rcy --cutoff-distance foobar',
-            'single-met-aromatic-query 1rcy --cutoff-angle foobar',
-            'single-met-aromatic-query 1rcy --vertices foo',
+            'pair ',
+            'pair 1rcy single-bridging-interaction-query 1rcy',
+            'pair 1rcy --cutoff-distance foobar',
+            'pair 1rcy --cutoff-angle foobar',
+            'pair 1rcy --vertices foo',
             '--batch ',
         ]
     )
