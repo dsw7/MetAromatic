a0e224177b11346b6862cdf706171140ef1426a4
David Weber <dsw7@sfu.ca>
2021-06-03 03:55:33 +0100

    Format

--- utils/parallel_processing.py
+++ utils/parallel_processing.py
@@ -45,20 +45,15 @@ class RunBatchQueries(MetAromatic):
             self.parameters['model']
         )
 
-        pdb_codes = self._read_batch_file()
-        self.number_pdb_codes = len(pdb_codes)
-
-        logging.info('Splitting list of pdb codes into %i chunks', self.parameters['threads'])
-        self.chunked_pdb_codes = [
-            pdb_codes[i::self.parameters['threads']] for i in range(self.parameters['threads'])
-        ]
-
-        self.client = MongoClient(DEFAULT_MONGO_HOST, DEFAULT_MONGO_PORT)
-        self.count = 0
-
         if self.parameters['threads'] > MAXIMUM_WORKERS:
             logging.warning('Number of selected workers exceeds maximum number of workers.')
             logging.warning('The thread pool will use a maximum of %i workers.', MAXIMUM_WORKERS)
+
+        self.pdb_codes = []
+        self.chunked_pdb_codes = []
+        self.count = 0
+        self.client = MongoClient(DEFAULT_MONGO_HOST, DEFAULT_MONGO_PORT)
+        self.bool_disable_workers = None
 
         self.batch_job_metadata = {
             'num_workers': self.parameters['threads'],
@@ -69,33 +64,47 @@ class RunBatchQueries(MetAromatic):
             'data_acquisition_date': datetime.now()
         }
 
-        self.bool_disable_workers = False
-        signal(SIGINT, self.disable_all_workers)
+        self._register_ipc_signals()
+        self._read_batch_file()
+        self._generate_chunks()
 
-    def _read_batch_file(self) -> list:
-        # Click does existence check - no need for try / except
-        logging.info('Imported pdb codes from file %s', self.parameters['path_batch_file'])
-
-        pdb_codes = []
-        with open(self.parameters['path_batch_file']) as batch_file:
-            for line in batch_file:
-                pdb_codes.extend(
-                    [row for row in split(r'(;|,|\s)\s*', line) if len(row) == LEN_PDB_CODE]
-                )
-        return pdb_codes
-
-    def disable_all_workers(self, ipc_signal, frame):
-        self.bool_disable_workers = True
+    def _disable_all_workers(self, ipc_signal, frame) -> None:
         logging.info('Detected SIGINT!')
         logging.info('Attempting to stop all workers!')
 
-    def worker_met_aromatic(self, chunked_list_pdb_codes):
+        self.bool_disable_workers = True
+
+    def _register_ipc_signals(self) -> None:
+        logging.info('Registering SIGINT to thread terminator')
+
+        self.bool_disable_workers = False
+        signal(SIGINT, self._disable_all_workers)
+
+    def _read_batch_file(self) -> None:
+        # Click does existence check - no need for try / except
+        logging.info('Imported pdb codes from file %s', self.parameters['path_batch_file'])
+
+        with open(self.parameters['path_batch_file']) as batch_file:
+            for line in batch_file:
+                self.pdb_codes.extend(
+                    [row for row in split(r'(;|,|\s)\s*', line) if len(row) == LEN_PDB_CODE]
+                )
+
+    def _generate_chunks(self) -> None:
+        logging.info('Splitting list of pdb codes into %i chunks', self.parameters['threads'])
+        self.chunked_pdb_codes = [
+            self.pdb_codes[i::self.parameters['threads']] for i in range(self.parameters['threads'])
+        ]
+
+    def worker_met_aromatic(self, chunk: list) -> None:
         collection = self.client[self.parameters['database']][self.parameters['collection']]
 
-        for code in chunked_list_pdb_codes:
+        for code in chunk:
+
             if self.bool_disable_workers:
                 logging.info('Received interrupt signal - stopping worker thread...')
                 break
+
             try:
                 results = self.get_met_aromatic_interactions(code)
             except Exception:  # catch remaining unhandled exceptions
@@ -110,9 +119,7 @@ class RunBatchQueries(MetAromatic):
         if self.parameters['database'] in self.client.list_database_names():
             if self.parameters['collection'] in self.client[self.parameters['database']].list_collection_names():
                 logging.error(
-                    'Database/collection pair %s.%s exists!',
-                    self.parameters['database'],
-                    self.parameters['collection']
+                    'Database/collection pair %s.%s exists!', self.parameters['database'], self.parameters['collection']
                 )
                 return EXIT_FAILURE
 
@@ -137,7 +144,7 @@ class RunBatchQueries(MetAromatic):
                 logging.info('Batch job execution time: %f s', execution_time)
 
                 self.batch_job_metadata['batch_job_execution_time'] = execution_time
-                self.batch_job_metadata['number_of_entries'] = self.number_pdb_codes
+                self.batch_job_metadata['number_of_entries'] = len(self.pdb_codes)
 
                 collection_info.insert(self.batch_job_metadata)
 
