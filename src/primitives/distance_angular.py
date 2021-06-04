9a2ef4855c6d3b811301921858c6bf00e9839bac
David Weber <dsw7@sfu.ca>
2021-06-04 03:46:11 +0100

    More descriptive naming

--- src/primitives/distance_angular.py
+++ src/primitives/distance_angular.py
@@ -3,26 +3,33 @@ from .math_utils.cross_product_method import vector_angle
 
 ROUND_TO_NUMBER = 3
 
-def apply_distance_angular_condition(midpoints, lone_pairs, cutoff_distance, cutoff_angle):
-    results = []
-    for dict_met in lone_pairs:
+def apply_distance_angular_condition(midpoints: list, lone_pairs: list, cutoff_distance: float, cutoff_angle: float) -> list:
+    met_aromatic_interactions = []
+
+    for lone_pair_data in lone_pairs:
         for midpoint in midpoints:
-            vector_v = midpoint[2] - dict_met['coords_sd']
-            norm_v = norm(vector_v)
+
+            v = midpoint[2] - lone_pair_data['coords_sd']
+            norm_v = norm(v)
+
             if norm_v <= cutoff_distance:
-                met_theta = vector_angle(vector_v, dict_met['vector_a'])
-                met_phi = vector_angle(vector_v, dict_met['vector_g'])
-                if (met_theta <= cutoff_angle) or (met_phi <= cutoff_angle):
-                    results.append({
+
+                met_theta_angle = vector_angle(v, lone_pair_data['vector_a'])
+                met_phi_angle = vector_angle(v, lone_pair_data['vector_g'])
+
+                if (met_theta_angle <= cutoff_angle) or (met_phi_angle <= cutoff_angle):
+                    result = {
                         'aromatic_residue': midpoint[1],
                         'aromatic_position': int(midpoint[0]),
-                        'methionine_position': int(dict_met['position']),
+                        'methionine_position': int(lone_pair_data['position']),
                         'norm': round(norm_v, ROUND_TO_NUMBER),
-                        'met_theta_angle': round(met_theta, ROUND_TO_NUMBER),
-                        'met_phi_angle': round(met_phi, ROUND_TO_NUMBER)
-                    })
+                        'met_theta_angle': round(met_theta_angle, ROUND_TO_NUMBER),
+                        'met_phi_angle': round(met_phi_angle, ROUND_TO_NUMBER)
+                    }
+                    met_aromatic_interactions.append(result)
+
                 else:
                     pass
             else:
                 pass
-    return results
+    return met_aromatic_interactions
