d56371d458701f212bf57d144f503ff1e88a28a1
David Weber <dsw7@sfu.ca>
2021-06-04 03:15:28 +0100

    Clean up

--- src/primitives/math_utils/cross_product_method.py
+++ src/primitives/math_utils/cross_product_method.py
@@ -1,35 +1,45 @@
-from numpy import degrees
-from numpy import linalg
-from numpy import cross
-from numpy import arccos
-from numpy import dot
+# pylint: disable=C0103 # Disable "Argument name "u" doesn't conform to snake_case naming style"
 
+from numpy import (
+    degrees,
+    linalg,
+    cross,
+    arccos,
+    dot,
+    ndarray
+)
 
-def vector_angle(vec_u, vec_v):
-    num = dot(vec_u, vec_v)
-    den = linalg.norm(vec_v) * linalg.norm(vec_u)
-    return degrees(arccos(num / den))
+def vector_angle(u: ndarray, v: ndarray) -> float:
+    numerator = dot(u, v)
+    denominator = linalg.norm(v) * linalg.norm(u)
+    return degrees(arccos(numerator / denominator))
 
-def unit_vec(vec):
-    return vec / linalg.norm(vec)
+def get_unit_vector(v: ndarray) -> ndarray:
+    return v / linalg.norm(v)
 
 class CrossProductMethod:
     """
     A class for computing the vectors parallel to MET SD lone pairs
     Methods associated with the class complete the vertices of a tetrahedron
     """
-    def __init__(self, terminal_a, midpoint, terminal_b):
+
+    def __init__(self, terminal_a: ndarray, midpoint: ndarray, terminal_b: ndarray) -> None:
+
         self.terminal_a = terminal_a
         self.midpoint = midpoint
         self.terminal_b = terminal_b
-        self.vec_u = self.terminal_a - self.midpoint
-        self.vec_v = self.terminal_b - self.midpoint
-        self.anti_parallel_vec = unit_vec(-0.5 * (unit_vec(self.vec_v) + unit_vec(self.vec_u)))
 
-    def get_vector_a(self):
-        cross_vec = cross(self.vec_u, self.vec_v)
-        return self.anti_parallel_vec + 2**0.5 * unit_vec(cross_vec)
+        self.u = self.terminal_a - self.midpoint
+        self.v = self.terminal_b - self.midpoint
 
-    def get_vector_g(self):
-        cross_vec = cross(self.vec_v, self.vec_u)
-        return self.anti_parallel_vec + 2**0.5 * unit_vec(cross_vec)
+        self.anti_parallel_vec = get_unit_vector(
+            -0.5 * (get_unit_vector(self.v) + get_unit_vector(self.u))
+        )
+
+    def get_vector_a(self) -> ndarray:
+        cross_vec = cross(self.u, self.v)
+        return self.anti_parallel_vec + 2**0.5 * get_unit_vector(cross_vec)
+
+    def get_vector_g(self) -> ndarray:
+        cross_vec = cross(self.v, self.u)
+        return self.anti_parallel_vec + 2**0.5 * get_unit_vector(cross_vec)
