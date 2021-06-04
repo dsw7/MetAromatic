bd0fe83e245c18c0b445855d2a3aa46ea81e995b
David Weber <dsw7@sfu.ca>
2021-06-04 04:58:08 +0100

    Update tests

--- src/primitives/math_utils/test_rodrigues_method.py
+++ src/primitives/math_utils/test_rodrigues_method.py
@@ -1,25 +1,22 @@
 # pylint: disable=W0201  # Disable "Attribute defined outside __init__"
 
-from numpy import array, around, array_equal
-from .cross_product_method import CrossProductMethod
+from numpy import (
+    array,
+    around,
+    array_equal
+)
 from .rodrigues_method import RodriguesMethod
 
-
-class TestVectorInterpolators:
+class TestRodriguesMethod:
 
     def setup_class(self) -> None:
         vertex_a = array([0.5, 0.5, 0.0])
         vertex_g = array([0.5, -0.5, 0.0])
         origin = array([0.0, 0.0, 0.0])
 
-        self.frame_cp = CrossProductMethod(vertex_a, origin, vertex_g)
-        self.frame_rm = RodriguesMethod(vertex_a, origin, vertex_g)
-
-    def test_cp_vector_a(self) -> None:
-        assert array_equal(around(self.frame_cp.get_vector_a(), decimals=2), [-1., 0., -1.41])
-
-    def test_cp_vector_g(self) -> None:
-        assert array_equal(around(self.frame_cp.get_vector_g(), decimals=2), [-1., 0., 1.41])
+        self.frame_rm = RodriguesMethod(
+            vertex_a, origin, vertex_g
+        )
 
     def test_rm_vector_a(self) -> None:
         assert array_equal(around(self.frame_rm.get_vector_a(), decimals=1), [-0.5, 0.0, -0.5])
