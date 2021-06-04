c85efe32cf2c587013f21caf3245cf3cf3499bf1
David Weber <dsw7@sfu.ca>
2021-06-04 03:33:29 +0100

    Clean up lone pairs

--- src/primitives/get_lone_pairs.py
+++ src/primitives/get_lone_pairs.py
@@ -1,14 +1,16 @@
+from typing import Union
 from itertools import groupby
 from operator import itemgetter
 from numpy import array
 from .math_utils.rodrigues_method import RodriguesMethod
 from .math_utils.cross_product_method import CrossProductMethod
 
+def get_lone_pairs(met_coordinates: list, model: str) -> Union[list, bool]:
+    lone_pairs = []
 
-def get_lone_pairs(methionine_coords, model):
-    lonepairs = []
-    for position, groups in groupby(methionine_coords, lambda entry: entry[5]):
+    for position, groups in groupby(met_coordinates, lambda entry: entry[5]):
         ordered = sorted(list(groups), key=itemgetter(2))
+
         coordinates_ce = array(ordered[0][6:9]).astype(float)
         coordinates_cg = array(ordered[1][6:9]).astype(float)
         coordinates_sd = array(ordered[2][6:9]).astype(float)
@@ -20,11 +22,11 @@ def get_lone_pairs(methionine_coords, model):
         else:
             return False
 
-        lonepairs.append({
+        lone_pairs.append({
             'vector_a': lonepair.get_vector_a(),
             'vector_g': lonepair.get_vector_g(),
             'coords_sd': coordinates_sd,
             'position': position
         })
 
-    return lonepairs
+    return lone_pairs
