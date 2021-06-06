16f603dd6f93b4786341f748f2155cfbcff72547
David Weber <dsw7@sfu.ca>
2021-06-06 14:27:47 +0100

    Fix regex pattern

--- MetAromatic/utils/primitives/regex_patterns.py
+++ MetAromatic/utils/primitives/regex_patterns.py
@@ -4,7 +4,7 @@ ATOMS_TRP = r'CD2|CE3|CZ2|CH2|CZ3|CE2'
 ATOMS_PHE = r'CD1|CE1|CZ|CG|CD2|CE2'
 
 def pattern_regex_met(chain: str) -> str:
-    return r'(ATOM.*({})\s+MET\s+{}\s)'.format(ATOMS_PHE, chain)
+    return r'(ATOM.*({})\s+MET\s+{}\s)'.format(ATOMS_MET, chain)
 
 def pattern_regex_phe(chain: str) -> str:
     return r'(ATOM.*({})\s+PHE\s+{}\s)'.format(ATOMS_PHE, chain)
