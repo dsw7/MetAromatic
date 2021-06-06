1b9ec208936d6f9a778dafa0305c27c1fc4dea2b
David Weber <dsw7@sfu.ca>
2021-06-06 14:14:22 +0100

    Remove f-string

--- MetAromatic/utils/primitives/regex_patterns.py
+++ MetAromatic/utils/primitives/regex_patterns.py
@@ -4,13 +4,13 @@ ATOMS_TRP = r'CD2|CE3|CZ2|CH2|CZ3|CE2'
 ATOMS_PHE = r'CD1|CE1|CZ|CG|CD2|CE2'
 
 def pattern_regex_met(chain: str) -> str:
-    return fr'(ATOM.*({ATOMS_MET})\s+MET\s+{chain}\s)'
+    return r'(ATOM.*({})\s+MET\s+{}\s)'.format(ATOMS_PHE, chain)
 
 def pattern_regex_phe(chain: str) -> str:
-    return fr'(ATOM.*({ATOMS_PHE})\s+PHE\s+{chain}\s)'
+    return r'(ATOM.*({})\s+PHE\s+{}\s)'.format(ATOMS_PHE, chain)
 
 def pattern_regex_tyr(chain: str) -> str:
-    return fr'(ATOM.*({ATOMS_TYR})\s+TYR\s+{chain}\s)'
+    return r'(ATOM.*({})\s+TYR\s+{}\s)'.format(ATOMS_TYR, chain)
 
 def pattern_regex_trp(chain: str) -> str:
-    return fr'(ATOM.*({ATOMS_TRP})\s+TRP\s+{chain}\s)'
+    return r'(ATOM.*({})\s+TRP\s+{}\s)'.format(ATOMS_TRP, chain)
