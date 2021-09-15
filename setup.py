2f6af2fd610375bd68469820ee8099f63281b03b
David Weber <dsw7@sfu.ca>
2021-09-14 21:21:23 -0700

    Base

--- setup.py
+++ setup.py
@@ -2,13 +2,9 @@ from setuptools import (
     setup,
     find_packages
 )
-#from os import path
-#
-#def get_path_to_commands() -> str:
-#    return path.join(path.dirname(__file__),
 
 setup(
     name = 'metaromatic',
     version='1.0',
-    packages=find_packages('MetAromatic')
+    packages=find_packages()
 )
