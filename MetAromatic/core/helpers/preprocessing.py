8df6827842fd0a05bee7c87ba9c3bd36c7f42b1a
David Weber <dsw7@sfu.ca>
2021-09-15 23:59:02 -0700

    Flatten regex:

--- MetAromatic/core/helpers/preprocessing.py
+++ MetAromatic/core/helpers/preprocessing.py
@@ -1,11 +1,22 @@
 from typing import Union
 from re import match
-from .regex_patterns import (
-    pattern_regex_met,
-    pattern_regex_phe,
-    pattern_regex_tyr,
-    pattern_regex_trp
-)
+
+ATOMS_MET = r'CE|SD|CG'
+ATOMS_TYR = r'CD1|CE1|CZ|CG|CD2|CE2'
+ATOMS_TRP = r'CD2|CE3|CZ2|CH2|CZ3|CE2'
+ATOMS_PHE = r'CD1|CE1|CZ|CG|CD2|CE2'
+
+def pattern_regex_met(chain: str) -> str:
+    return r'(ATOM.*({})\s+MET\s+{}\s)'.format(ATOMS_MET, chain)
+
+def pattern_regex_phe(chain: str) -> str:
+    return r'(ATOM.*({})\s+PHE\s+{}\s)'.format(ATOMS_PHE, chain)
+
+def pattern_regex_tyr(chain: str) -> str:
+    return r'(ATOM.*({})\s+TYR\s+{}\s)'.format(ATOMS_TYR, chain)
+
+def pattern_regex_trp(chain: str) -> str:
+    return r'(ATOM.*({})\s+TRP\s+{}\s)'.format(ATOMS_TRP, chain)
 
 def get_raw_data_from_file(filepath: str) -> Union[bool, list]:
     raw_data = []
