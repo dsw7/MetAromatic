78583ac1f8ab97c057a84b6928d1a25360fdf389
David Weber <dsw7@sfu.ca>
2021-09-15 23:32:15 -0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(
     name = 'MetAromatic',
     version='1.0',
-    packages=find_packages(),
     packages=[
         'MetAromatic/core',
         'MetAromatic/core/helpers'
