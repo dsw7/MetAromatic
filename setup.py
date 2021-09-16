13ae5e877fb9e442cc53941c490853cafd85f383
David Weber <dsw7@sfu.ca>
2021-09-15 21:24:32 -0700

    Update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import (
 setup(
     name = 'MetAromatic',
     version='1.0',
-    packages=find_packages('commands'),
+    packages=find_packages(),
     install_requires=[
         'pymongo',
         'networkx',
