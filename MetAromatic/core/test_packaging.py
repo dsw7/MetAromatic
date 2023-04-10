a6005f2a48068506d5fe70f1ac3776e40417b120
David Weber <dsw7@sfu.ca>
2023-04-10 01:32:09 -0700

    Update test_packaging.py

--- MetAromatic/core/test_packaging.py
+++ MetAromatic/core/test_packaging.py
@@ -1,9 +1,10 @@
 # pylint: disable=C0415   # Disable "Import outside toplevel"
 
-from pytest import mark
+import pytest
 
-@mark.test_packaging
+@pytest.mark.test_packaging
 def test_import_pair() -> None:
+
     arguments = {
         'cutoff_distance': 4.9,
         'cutoff_angle': 109.5,
@@ -12,10 +13,9 @@ def test_import_pair() -> None:
     }
 
     from MetAromatic.core.pair import MetAromatic
-    results = MetAromatic(**arguments).get_met_aromatic_interactions('1rcy')
-    assert results['_id'] == '1rcy'
+    assert MetAromatic(**arguments).get_met_aromatic_interactions('1rcy')['_id'] == '1rcy'
 
-@mark.test_packaging
+@pytest.mark.test_packaging
 def test_import_bridge() -> None:
     arguments = {
         'cutoff_distance': 6.0,
@@ -25,5 +25,4 @@ def test_import_bridge() -> None:
     }
 
     from MetAromatic.core.bridge import GetBridgingInteractions
-    results = GetBridgingInteractions(**arguments).get_bridging_interactions('7mdh', 4)
-    assert results['_id'] == '7mdh'
+    assert GetBridgingInteractions(**arguments).get_bridging_interactions('7mdh', 4)['_id'] == '7mdh'
