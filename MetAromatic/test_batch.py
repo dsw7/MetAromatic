20acca17016121518f2c8d05a65934312a150235
David Weber <dsw7@sfu.ca>
2023-04-14 02:20:20 -0700

    Fix batch tests

--- MetAromatic/test_batch.py
+++ MetAromatic/test_batch.py
@@ -46,6 +46,9 @@ class TestParallelProcessing:
 
         self.num_coronavirus_entries = 9
 
+        info_collection = self.client[self.db_name][self.col_name + '_info']
+        self.results_info = list(info_collection.find())[0]
+
     def teardown_class(self):
         self.client.drop_database(self.db_name)
 
@@ -53,60 +56,50 @@ class TestParallelProcessing:
         assert len(list(self.cursor.find())) == self.num_coronavirus_entries
 
     def test_correct_exit_code_2ca1(self):
-        assert list(self.cursor.find({'_id': '2ca1'}))[0]['exit_code'] == EX_OK
+        assert list(self.cursor.find({'_id': '2ca1'}))[0]['ok']
 
     def test_correct_exit_code_2fyg(self):
-        assert list(self.cursor.find({'_id': '2fyg'}))[0]['exit_code'] == EX_OK
+        assert list(self.cursor.find({'_id': '2fyg'}))[0]['ok']
 
     def test_correct_exit_status_2ca1(self):
-        assert list(self.cursor.find({'_id': '2ca1'}))[0]['exit_status'] == 'Success'
+        assert list(self.cursor.find({'_id': '2ca1'}))[0]['status'] == 'Success'
 
     def test_correct_exit_status_2fyg(self):
-        assert list(self.cursor.find({'_id': '2fyg'}))[0]['exit_status'] == 'Success'
+        assert list(self.cursor.find({'_id': '2fyg'}))[0]['status'] == 'Success'
 
     def test_correct_exit_code_1xak(self):
-
-        assert list(
-            self.cursor.find({'_id': '1xak'})
-        )[0]['exit_code'] != EX_OK
+        assert not list(self.cursor.find({'_id': '1xak'}))[0]['ok']
 
     def test_correct_exit_code_2fxp(self):
-
-        assert list(
-            self.cursor.find({'_id': '2fxp'})
-        )[0]['exit_code'] != EX_OK
+        assert not list(self.cursor.find({'_id': '2fxp'}))[0]['ok']
 
     def test_correct_exit_code_6mwm(self):
-
-        assert list(
-            self.cursor.find({'_id': '6mwm'})
-        )[0]['exit_code'] != EX_OK
+        assert not list(self.cursor.find({'_id': '6mwm'}))[0]['ok']
 
     def test_correct_exit_code_2cme(self):
-
-        assert list(
-            self.cursor.find({'_id': '2cme'})
-        )[0]['exit_code'] != EX_OK
+        assert not list(self.cursor.find({'_id': '2cme'}))[0]['ok']
 
     def test_correct_exit_code_spam(self):
-
-        assert list(
-            self.cursor.find({'_id': 'spam'})
-        )[0]['exit_code'] != EX_OK
+        assert not list(self.cursor.find({'_id': 'spam'}))[0]['ok']
 
     def test_info_file_in_results(self):
         collections = self.client[self.db_name].list_collection_names()
-
         assert self.col_name + '_info' in collections
 
     def test_info_file_correct_num_workers(self):
-        info_collection = self.client[self.db_name][self.col_name + '_info']
-
-        assert list(info_collection.find())[0]['num_workers'] == self.threads
+        assert self.results_info['num_workers'] == self.threads
 
     def test_info_file_correct_number_of_entries(self):
-        info_collection = self.client[self.db_name][self.col_name + '_info']
+        assert self.results_info['number_of_entries'] == self.num_coronavirus_entries
 
-        assert list(
-            info_collection.find()
-        )[0]['number_of_entries'] == self.num_coronavirus_entries
+    def test_info_file_correct_chain(self):
+        assert self.results_info['chain'] == 'A'
+
+    def test_info_file_correct_model(self):
+        assert self.results_info['model'] == 'cp'
+
+    def test_info_file_correct_cutoff_distance(self):
+        assert self.results_info['cutoff_distance'] == 4.9
+
+    def test_info_file_correct_cutoff_angle(self):
+        assert self.results_info['cutoff_angle'] == 109.5
