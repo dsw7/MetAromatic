1d80e18c8fbaca14b24f392adc3de4ad19597532
David Weber <dsw7@sfu.ca>
2023-07-09 02:53:43 -0700

    Fix midpoint test

--- tests/test_get_hexagon_midpoints.py
+++ tests/test_get_hexagon_midpoints.py
@@ -1,26 +1,7 @@
-from MetAromatic.get_aromatic_midpoints import get_hexagon_midpoints
+from MetAromatic.get_aromatic_midpoints import get_midpoints
 
-HEXAGON_COORDINATES = [
-    (0.866, 0.5, 0.0),
-    (0.866, -0.5, 0.0),
-    (0.0, -1.0, 0.0),
-    (-0.866, -0.5, 0.0),
-    (-0.866, 0.5, 0.0),
-    (0.0, 1.0, 0.0)
-]
-
-HEXAGON_MIDPOINTS = {
-    (0.433, 0.75, 0.0),
-    (-0.433, 0.75, 0.0),
-    (-0.866, 0.0, 0.0),
-    (-0.433, -0.75, 0.0),
-    (0.433, -0.75, 0.0),
-    (0.866, 0.0, 0.0)
-}
+HEXAGON_COORDINATES = [0.866, 0.866, 0.0, -0.866, -0.866, 0.0]
+HEXAGON_MIDPOINTS = [0.866, 0.433, -0.433, -0.866, -0.433, 0.433]
 
 def test_get_hexagon_midpoints() -> None:
-    coordinates = [list(i) for i in zip(*HEXAGON_COORDINATES)]
-
-    assert {
-        tuple(i) for i in zip(*get_hexagon_midpoints(*coordinates))
-    } == HEXAGON_MIDPOINTS
+    assert get_midpoints(HEXAGON_COORDINATES) == HEXAGON_MIDPOINTS
