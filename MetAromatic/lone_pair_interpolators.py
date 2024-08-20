8002fb27434188c435b6ada5e0c1ab6666b6882e
David Weber <dds3443@hotmail.com>
2024-08-19 22:19:52 -0700

    Move linear algebraic helpers to utils

--- MetAromatic/lone_pair_interpolators.py
+++ MetAromatic/lone_pair_interpolators.py
@@ -1,20 +1,15 @@
-# pylint: disable=C0103 # Disable "Argument name "u" doesn't conform to snake_case naming style"
-
 import numpy as np
+from .utils import get_unit_vector, get_3x3_identity_matrix
 
 SCAL1 = np.sin(np.pi / 2)
 SCAL2 = 1 - np.cos(np.pi / 2)
 ROOT_2 = 2**0.5
 
 
-def get_unit_vector(v: np.ndarray) -> np.ndarray:
-    return v / np.linalg.norm(v)
-
-
 class CrossProductMethod:
     """
-    A class for computing the vectors parallel to MET SD lone pairs Methods associated with the class complete
-    the vertices of a tetrahedron
+    A class for computing the vectors parallel to MET SD lone pairs Methods
+    associated with the class complete the vertices of a tetrahedron
     """
 
     def __init__(
@@ -42,13 +37,15 @@ class CrossProductMethod:
 
 class RodriguesMethod:
     """
-    Here I use Rodrigues' rotation formula for completing the vertices of a regular tetrahedron. To start, we
-    know two vertices of a tetrahedron, A and B, in addition to knowing the origin O. So we map A and B to the
-    origin of the frame in which the tetrahedron resides by computing u = A - O and v = B - O. The directions
-    of u, v are then flipped by scaling u, v by -1. We then take -u, -v and rotate the vectors by 90 degrees
-    about k, where k is the line of intersection between the two orthogonal planes of the tetrahedron. These
-    rotated vectors now describe the position of the remaining coordinates C, D. We have our tetrahedron with
-    vertices A, B, C, D and the origin O.
+    Here I use Rodrigues' rotation formula for completing the vertices of a regular
+    tetrahedron. To start, we know two vertices of a tetrahedron, A and B, in
+    addition to knowing the origin O. So we map A and B to the origin of the frame
+    in which the tetrahedron resides by computing u = A - O and v = B - O. The
+    directions of u, v are then flipped by scaling u, v by -1. We then take -u, -v
+    and rotate the vectors by 90 degrees about k, where k is the line of
+    intersection between the two orthogonal planes of the tetrahedron. These rotated
+    vectors now describe the position of the remaining coordinates C, D. We have our
+    tetrahedron with vertices A, B, C, D and the origin O.
     """
 
     def __init__(
@@ -66,21 +63,23 @@ class RodriguesMethod:
         r = 0.5 * (self.u + self.v)
 
         # Then find unit vector of r
-        r_hat = r / np.linalg.norm(r)
+        r_hat = get_unit_vector(r)
 
         # Get components of the unit vector r
         r_hat_x = r_hat[0]
         r_hat_y = r_hat[1]
         r_hat_z = r_hat[2]
 
-        # Get the W array
-        W = np.array(
+        # Get the linear transformation matrix
+        mat_transformation = np.array(
             [[0, -r_hat_z, r_hat_y], [r_hat_z, 0, -r_hat_x], [-r_hat_y, r_hat_x, 0]]
         )
 
         # Then construct Rodrigues rotation array
         self.rodrigues_rotation_matrix = (
-            np.array(np.eye(3)) + (SCAL1 * W) + (SCAL2 * np.matmul(W, W))
+            get_3x3_identity_matrix()
+            + (SCAL1 * mat_transformation)
+            + (SCAL2 * np.matmul(mat_transformation, mat_transformation))
         )
 
     # Note that I flipped these methods to match previous algorithm
--- MetAromatic/utils.py
+++ MetAromatic/utils.py
@@ -1,5 +1,6 @@
 from functools import cache
 from os import get_terminal_size
+from numpy import ndarray, linalg, eye
 
 
 @cache
@@ -14,3 +15,12 @@ def get_separator() -> str:
 
 def print_separator() -> None:
     print(get_separator())
+
+
+def get_unit_vector(v: ndarray) -> ndarray:
+    return v / linalg.norm(v)
+
+
+@cache
+def get_3x3_identity_matrix() -> ndarray:
+    return eye(3)
