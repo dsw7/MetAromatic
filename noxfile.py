72e0967a890a6dd816235873926db0952a948aa6
David Weber <dds3443@hotmail.com>
2024-08-21 02:00:56 -0700

    Black on noxfile

--- noxfile.py
+++ noxfile.py
@@ -2,23 +2,24 @@ from pathlib import Path
 from glob import glob
 from nox import session, sessions
 
-PATH_WHL = Path(__file__).resolve().parent / 'dist' / '*.whl'
-PATH_TESTS = Path(__file__).resolve().parent / 'tests'
+PATH_WHL = Path(__file__).resolve().parent / "dist" / "*.whl"
+PATH_TESTS = Path(__file__).resolve().parent / "tests"
 
-@session(python='3.9')
+
+@session(python="3.11")
 def met_aromatic_tests(session: sessions.Session) -> None:
 
-    session.install('build')
-    command = 'python3 -m build'
+    session.install("build")
+    command = "python3 -m build"
     session.run(*command.split())
 
     whl = glob(str(PATH_WHL))
 
     if len(whl) > 1:
-        raise Exception('More than one wheel file exists. Cannot proceed')
+        raise Exception("More than one wheel file exists. Cannot proceed")
 
-    session.install(whl[0], '--force-reinstall')
+    session.install(whl[0], "--force-reinstall")
 
-    session.install('pytest', 'typing_extensions')
-    command = f'pytest -vs {PATH_TESTS}'
+    session.install("pytest", "typing_extensions")
+    command = f"pytest -vs {PATH_TESTS}"
     session.run(*command.split())
