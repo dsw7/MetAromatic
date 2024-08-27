e586a61c517852be2778e4fafb89dedd249e48ad
David Weber <dds3443@hotmail.com>
2024-08-27 02:06:58 -0700

    Update test_get_hexagon_midpoints.py

--- tests/test_get_hexagon_midpoints.py
+++ tests/test_get_hexagon_midpoints.py
@@ -1,8 +1,8 @@
 from MetAromatic.get_aromatic_midpoints import get_midpoints
 
-HEXAGON_COORDINATES = [0.866, 0.866, 0.0, -0.866, -0.866, 0.0]
-HEXAGON_MIDPOINTS = [0.866, 0.433, -0.433, -0.866, -0.433, 0.433]
-
 
 def test_get_hexagon_midpoints() -> None:
-    assert get_midpoints(HEXAGON_COORDINATES) == HEXAGON_MIDPOINTS
+    hexagon_coords = [0.866, 0.866, 0.0, -0.866, -0.866, 0.0]
+    hexagon_midpoints = [0.866, 0.433, -0.433, -0.866, -0.433, 0.433]
+
+    assert get_midpoints(hexagon_coords) == hexagon_midpoints
