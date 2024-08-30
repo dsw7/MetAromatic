8804404706c96b9e0d0e97bde7de2890bdb664d3
David Weber <dds3443@hotmail.com>
2024-08-29 23:13:30 -0700

    Update bridge api test

--- tests/test_api_bridge.py
+++ tests/test_api_bridge.py
@@ -1,67 +1,55 @@
 import pytest
+from utils import Defaults
 from MetAromatic import get_bridges
 from MetAromatic.aliases import Models
 from MetAromatic.errors import SearchError
-from MetAromatic.models import MetAromaticParams
-
 
 @pytest.mark.parametrize(
-    "code, distance, angle, model, error",
+    "code, error",
     [
-        ("2rcy", 4.95, 109.5, "cp", "No MET residues"),
-        ("3nir", 4.95, 109.5, "cp", "No MET residues"),
-        ("6mwm", 4.95, 109.5, "cp", "No PHE/TYR/TRP residues"),
+        ("2rcy", "No MET residues"),
+        ("3nir", "No MET residues"),
+        ("6mwm", "No PHE/TYR/TRP residues"),
     ],
 )
-def test_bridge_missing_residues(
-    code: str,
-    distance: float,
-    angle: float,
-    model: Models,  # Note the 'pc' passed above would technically fail type checker
-    error: str,
-) -> None:
+def test_bridge_missing_residues(code: str, error: str, defaults: Defaults) -> None:
     with pytest.raises(SearchError, match=error):
-        get_bridges(
-            code=code,
-            params=MetAromaticParams(
-                chain="A",
-                cutoff_angle=angle,
-                cutoff_distance=distance,
-                model=model,
-            ),
-            vertices=4,
-        )
+        get_bridges(code=code, vertices=4, **defaults)
+
+
+def test_bridge_invalid_pdb_code(defaults: Defaults) -> None:
+    with pytest.raises(SearchError, match="Invalid PDB entry"):
+        get_bridges(code="abcd", vertices=4, **defaults)
 
 
 @pytest.mark.parametrize(
     "code, distance, angle, model, error",
     [
-        ("1rcy", -0.01, 109.5, "cp", "Invalid cutoff distance"),
-        ("1rcy", 4.95, -60.0, "cp", "Invalid cutoff angle"),
-        ("1rcy", 4.95, 720.0, "cp", "Invalid cutoff angle"),
-        ("abcd", 4.95, 109.5, "cp", "Invalid PDB entry"),
-        ("1rcy", 4.95, 109.5, "pc", "Invalid model"),
-        ("1rcy", "4.95", 109.5, "cp", "Cutoff distance must be a valid float"),
-        ("1rcy", 4.95, "109.5", "cp", "Cutoff angle must be a valid float"),
-        ("1rcy", 4.95, 109.5, 25, "Model must be a valid string"),
+        ("1rcy", -0.01, 109.5, "cp", "cutoff_distance: Input should be greater than 0"),
+        ("1rcy", 4.95, -60.0, "cp", "cutoff_angle: Input should be greater than 0"),
+        ("1rcy", 4.95, 720.0, "cp", "cutoff_angle: Input should be less than 360"),
+        ("1rcy", 4.95, 109.5, "pc", "model: Input should be 'cp' or 'rm'"),
+        (
+            "1rcy",
+            "4.95",
+            109.5,
+            "cp",
+            "cutoff_distance: Input should be a valid number",
+        ),
+        ("1rcy", 4.95, "109.5", "cp", "cutoff_angle: Input should be a valid number"),
+        ("1rcy", 4.95, 109.5, 25, "model: Input should be 'cp' or 'rm'"),
     ],
 )
 def test_bridge_validation(
-    code: str,
-    distance: float,
-    angle: float,
-    model: Models,  # Note the 'pc' passed above would technically fail type checker
-    error: str,
+    code: str, distance: float, angle: float, model: Models, error: str
 ) -> None:
     with pytest.raises(SearchError, match=error):
         get_bridges(
             code=code,
-            params=MetAromaticParams(
-                chain="A",
-                cutoff_angle=angle,
-                cutoff_distance=distance,
-                model=model,
-            ),
+            cutoff_angle=angle,
+            cutoff_distance=distance,
+            chain="A",
+            model=model,
             vertices=4,
         )
 
@@ -70,6 +58,6 @@ def test_bridge_validation(
     "code, error",
     [("1a5r", "No Met-aromatic interactions"), ("1rcy", "Found no bridges")],
 )
-def test_no_results(code: str, error: str, ma_params: MetAromaticParams) -> None:
+def test_no_results(code: str, error: str, defaults: Defaults) -> None:
     with pytest.raises(SearchError, match=error):
-        get_bridges(code=code, params=ma_params, vertices=4)
+        get_bridges(code=code, vertices=4, **defaults)
