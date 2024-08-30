758394ce90556f14e372acf5fd945c9149dee6db
David Weber <dds3443@hotmail.com>
2024-08-29 22:51:39 -0700

    Separate tests

--- tests/test_api_bridge.py
+++ tests/test_api_bridge.py
@@ -8,12 +8,37 @@ from MetAromatic.models import MetAromaticParams
 @pytest.mark.parametrize(
     "code, distance, angle, model, error",
     [
-        ("1rcy", -0.01, 109.5, "cp", "Invalid cutoff distance"),
-        ("1rcy", 4.95, -60.0, "cp", "Invalid cutoff angle"),
-        ("1rcy", 4.95, 720.0, "cp", "Invalid cutoff angle"),
         ("2rcy", 4.95, 109.5, "cp", "No MET residues"),
         ("3nir", 4.95, 109.5, "cp", "No MET residues"),
         ("6mwm", 4.95, 109.5, "cp", "No PHE/TYR/TRP residues"),
+    ],
+)
+def test_bridge_missing_residues(
+    code: str,
+    distance: float,
+    angle: float,
+    model: Models,  # Note the 'pc' passed above would technically fail type checker
+    error: str,
+) -> None:
+    with pytest.raises(SearchError, match=error):
+        get_bridges(
+            code=code,
+            params=MetAromaticParams(
+                chain="A",
+                cutoff_angle=angle,
+                cutoff_distance=distance,
+                model=model,
+            ),
+            vertices=4,
+        )
+
+
+@pytest.mark.parametrize(
+    "code, distance, angle, model, error",
+    [
+        ("1rcy", -0.01, 109.5, "cp", "Invalid cutoff distance"),
+        ("1rcy", 4.95, -60.0, "cp", "Invalid cutoff angle"),
+        ("1rcy", 4.95, 720.0, "cp", "Invalid cutoff angle"),
         ("abcd", 4.95, 109.5, "cp", "Invalid PDB entry"),
         ("1rcy", 4.95, 109.5, "pc", "Invalid model"),
         ("1rcy", "4.95", 109.5, "cp", "Cutoff distance must be a valid float"),
@@ -21,7 +46,7 @@ from MetAromatic.models import MetAromaticParams
         ("1rcy", 4.95, 109.5, 25, "Model must be a valid string"),
     ],
 )
-def test_invalid_inputs(
+def test_bridge_validation(
     code: str,
     distance: float,
     angle: float,
