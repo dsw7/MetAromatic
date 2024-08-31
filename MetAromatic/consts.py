e5e5a2936ae636fa9bdd3cb16c0c22118cdd3722
David Weber <dds3443@hotmail.com>
2024-08-31 02:01:48 -0700

    Add help messages

--- MetAromatic/consts.py
+++ MetAromatic/consts.py
@@ -1,4 +1,30 @@
+from enum import Enum
 from numpy import sin, cos, pi
+
+
+# CLI
+class Help(Enum):
+
+    CMD_BATCH = "Run a Met-aromatic query batch job."
+    CMD_BRIDGE = "Run a bridging interaction query on a single PDB entry."
+    CMD_PAIR = "Run a Met-aromatic query against a single PDB entry."
+    CMD_READ_LOCAL = "Run a Met-aromatic query against a local PDB file."
+
+    ANGLE = "Specify a cutoff angle in degrees."
+    CHAIN = "Specify a chain ID."
+    DIST = "Specify a cutoff distance in Angstroms."
+    MODEL = "Specify a lone pair interpolation model."
+
+    COLL = "Specify MongoDB collection to use."
+    DB = "Specify MongoDB database to use."
+    HOST = "Specify host name."
+    OVERWRITE = "Specify whether to overwrite collection."
+    PASSWORD = "Specify MongoDB password if authentication is enabled."
+    PORT = "Specify MongoDB TCP connection port."
+    THREADS = "Specify number of workers to use."
+    USERNAME = "Specify MongoDB username if authentication is enabled."
+    VERTICES = "Specify number of vertices."
+
 
 # Linear algebra
 # See https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula "Matrix notation" section
